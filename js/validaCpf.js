19d5111eff38333b300b3e01baafc3308346cd3f
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2024-08-07 06:52:45 -0300

    upd

--- js/validaCpf.js
+++ js/validaCpf.js
@@ -1,15 +1,13 @@
 export default function ehUmCpf(campo){
     const cpf = campo.value.replace(/\.|-/g,"")
-    if(validaNumerosRepitidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)){
-        console.log('Esse CPF não existe!')
-    } else {
-        console.log('Existe!')
-    }
+    if(validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)){
+        campo.setCustomValidity('Esse CPF não é valido.')
+    } 
 
 }
 
-function validaNumerosRepitidos(cpf){
-    const numerosRepitidos = [
+function validaNumerosRepetidos(cpf) {
+    const numerosRepetidos = [
         '00000000000',
         '11111111111',
         '22222222222',
@@ -19,41 +17,45 @@ function validaNumerosRepitidos(cpf){
         '66666666666',
         '77777777777',
         '88888888888',
-        '99999999999',
+        '99999999999'
     ]
 
-    return numerosRepitidos.includes(cpf)
+    return numerosRepetidos.includes(cpf)
 }
 
-function validaPrimeiroDigito(cpf){
-    let soma = 0
-    let multiplicador = 10
-    for(let tamanho = 0; tamanho <9; tamanho++){
-        soma += cpf[tamanho] * multiplicador
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
         multiplicador--
     }
 
-    soma = (soma * 10) % 11
-    if (soma == 10 || soma == 11){
-        soma = 0
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
     }
 
-    return soma != cpf[9]
+    return soma != cpf[9];
 }
 
 
-function validaSegundoDigito(cpf){
-    let soma = 0
-    let multiplicador = 11
-    for(let tamanho = 0; tamanho <10; tamanho++){
-        soma += cpf[tamanho] * multiplicador
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
         multiplicador--
     }
 
-    soma = (soma * 10) % 11
-    if (soma == 10 || soma == 11){
-        soma = 0
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
     }
 
-    return soma != cpf[10]
+    return soma != cpf[10];
 }
